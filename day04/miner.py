25d03c24542a8d10a90957c1f6dc2fb3435556f3
Mike Killpack <mkillpack@gmail.com>
2015-12-06 17:48:12 -0700

    Day 4 complete

---
+++ day04/miner.py
@@ -0,0 +1,21 @@
+from hashlib import md5
+
+def find_number(key, pattern):
+	found = False
+	key_bytes = key.encode()
+	number = 1
+	while True:
+		hasher = md5(key_bytes)
+		hasher.update(str(number).encode())
+		if hasher.hexdigest().startswith(pattern):
+			return number
+		number += 1
+
+# TODO: these tests take too long to run every time
+# assert(find_number('abcdef', '0' * 5) == 609043)
+# assert(find_number('pqrstuv', '0' * 5) == 1048970)
+
+key = 'iwrupvqb'
+
+print('Five zeroes:', find_number(key, '0' * 5))
+print('Six zeroes: ', find_number(key, '0' * 6))

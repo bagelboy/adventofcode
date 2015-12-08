a2719168c48bcff2894d399a7811fdbfce55fe88
Mike Killpack <mkillpack@gmail.com>
2015-12-07 23:33:51 -0700

    Day 8 complete

---
+++ day08/part1.py
@@ -0,0 +1,24 @@
+import re
+
+tokens = r'(?P<CHAR>\\x[0-9a-f][0-9a-f])|(?P<QUOTE>\\\")|(?P<BS>\\\\)'
+
+def memory_characters(string):
+    length = len(string)
+    if string.startswith(r'"') and string.endswith(r'"'):
+        length -= 2
+    for match in re.finditer(tokens, string):
+        token = match.lastgroup
+        if token == 'QUOTE' or token == 'BS':
+            length -= 1 # from 2 chars to 1
+        elif token == 'CHAR':
+            length -= 3 # from 4 chars to 1
+    return length
+
+total = 0
+with open('input') as input:
+    for line in input:
+        string = line.rstrip()
+        total += len(string)
+        total -= memory_characters(string)
+
+print(total)
---
+++ day08/part2.py
@@ -0,0 +1,21 @@
+import re
+
+tokens = r'(?P<QUOTE>")|(?P<BS>\\)'
+
+def memory_characters(string):
+    length = len(string)
+    length += 2
+    for match in re.finditer(tokens, string):
+        token = match.lastgroup
+        if token == 'QUOTE' or token == 'BS':
+            length += 1
+    return length
+
+total = 0
+with open('input') as input:
+    for line in input:
+        string = line.rstrip()
+        total -= len(string)
+        total += memory_characters(string)
+
+print(total)

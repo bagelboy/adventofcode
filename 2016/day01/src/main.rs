e65e1f1b494d536d7ebfe42293c11f1177eed5b8
Mike Killpack <mkillpack@gmail.com>
2016-12-03 02:20:45 -0700

    Refactor to solve day one, part two

--- 2016/day01/src/main.rs
+++ 2016/day01/src/main.rs
@@ -1,63 +1,31 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io;
 use std::io::Read;
 
-struct Point {
-    x: i32,
-    y: i32,
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+enum Direction {
+    Left,
+    Right,
 }
 
-enum Direction {
+enum Heading {
     North,
     East,
     South,
     West,
 }
 
-#[derive(Debug, PartialEq, Eq, Clone)]
-enum Turn {
-    Left,
-    Right,
-}
-
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Debug, Eq, PartialEq)]
 struct Instruction {
-    turn: Turn,
-    distance: i32,
+    turn_direction: Direction,
+    blocks: i32,
 }
 
-fn distance(p1: &Point, p2: &Point) -> u32 {
-    ((p1.x - p2.x).abs() as u32) + ((p1.y - p2.y).abs() as u32)
-}
-
-fn direction_after_turn(direction: Direction, turn: Turn) -> Direction {
-    match direction {
-        Direction::North => {
-            if turn == Turn::Left {
-                return Direction::West;
-            }
-            return Direction::East;
-        }
-        Direction::East => {
-            if turn == Turn::Left {
-                return Direction::North;
-            }
-            return Direction::South;
-
-        }
-        Direction::South => {
-            if turn == Turn::Left {
-                return Direction::East;
-            }
-            return Direction::West;
-        }
-        Direction::West => {
-            if turn == Turn::Left {
-                return Direction::South;
-            }
-            return Direction::North;
-        }
-    }
+#[derive(Clone, Hash, Eq, PartialEq)]
+struct Block {
+    x: i32,
+    y: i32,
 }
 
 fn read_input() -> Result<String, io::Error> {
@@ -66,108 +34,166 @@ fn read_input() -> Result<String, io::Error> {
     Ok(input)
 }
 
-fn parse_instructions(input: String) -> Result<Vec<Instruction>, io::Error> {
-    let directions = input.trim()
+fn parse_instructions(input: String) -> Result<Vec<Instruction>, String> {
+    let instructions = input.trim()
         .split(", ")
-        .map(|d| {
-            let values = d.split_at(1);
+        .map(|i| {
+            let values = i.split_at(1);
             Instruction {
-                turn: match values.0 {
-                    "L" => Turn::Left,
-                    _ => Turn::Right,
+                turn_direction: match values.0 {
+                    "L" => Direction::Left,
+                    "R" => Direction::Right,
+                    _ => panic!("invalid turn"),
                 },
-                distance: values.1.parse::<i32>().unwrap(),
+                blocks: values.1.parse::<i32>().expect("invalid number of blocks"),
             }
         })
         .collect();
 
-    Ok(directions)
+    Ok(instructions)
 }
 
-fn follow_directions(directions: &Vec<Instruction>,
-                     starting_point: &Point,
-                     starting_direction: Direction)
-                     -> Point {
-    let mut x = starting_point.x;
-    let mut y = starting_point.y;
-    let mut direction = starting_direction;
+fn find_distance(instructions: Vec<Instruction>) -> Result<i32, String> {
+    let mut position = Block { x: 0, y: 0 };
+    let mut heading = Heading::North;
 
-    for d in directions {
-        let new_direction = direction_after_turn(direction, d.turn.clone());
-        match new_direction {
-            Direction::North => {
-                y += d.distance;
-            }
-            Direction::East => {
-                x += d.distance;
-            }
-            Direction::South => {
-                y -= d.distance;
-            }
-            Direction::West => {
-                x -= d.distance;
-            }
-        }
-        direction = new_direction;
+    for i in instructions {
+        heading = move_position(&mut position, &heading, &i);
     }
 
-    Point { x: x, y: y }
+    Ok(position.x.abs() + position.y.abs())
+}
+
+fn find_revisited_distance(instructions: Vec<Instruction>) -> Option<i32> {
+    let mut position = Block { x: 0, y: 0 };
+    let mut heading = Heading::North;
+    let mut visited: HashSet<(i32, i32)> = HashSet::new();
+
+    for i in instructions {
+        heading = match move_and_track(&mut position, &mut visited, &heading, &i) {
+            Some(heading) => heading,
+            None => break,
+        }
+    }
+
+    if !visited.contains(&(position.x, position.y)) {
+        return None;
+    }
+    Some(position.x.abs() + position.y.abs())
+}
+
+fn calculate_new_heading(current_heading: &Heading, turn_direction: &Direction) -> Heading {
+    match (current_heading, turn_direction) {
+        (&Heading::North, &Direction::Right) => Heading::East,
+        (&Heading::East, &Direction::Right) => Heading::South,
+        (&Heading::South, &Direction::Right) => Heading::West,
+        (&Heading::West, &Direction::Right) => Heading::North,
+
+        (&Heading::North, &Direction::Left) => Heading::West,
+        (&Heading::East, &Direction::Left) => Heading::North,
+        (&Heading::South, &Direction::Left) => Heading::East,
+        (&Heading::West, &Direction::Left) => Heading::South,
+    }
+}
+
+fn move_position(position: &mut Block,
+                 current_heading: &Heading,
+                 instruction: &Instruction)
+                 -> Heading {
+    let heading = calculate_new_heading(current_heading, &instruction.turn_direction);
+
+    match heading {
+        Heading::North => {
+            position.y += instruction.blocks;
+        }
+        Heading::East => {
+            position.x += instruction.blocks;
+        }
+        Heading::South => {
+            position.y -= instruction.blocks;
+        }
+        Heading::West => {
+            position.x -= instruction.blocks;
+        }
+    }
+
+    heading
+}
+
+fn move_and_track(position: &mut Block,
+                  visited: &mut HashSet<(i32, i32)>,
+                  current_heading: &Heading,
+                  instruction: &Instruction)
+                  -> Option<Heading> {
+    let heading = calculate_new_heading(current_heading, &instruction.turn_direction);
+
+    if !visited.contains(&(position.x, position.y)) {
+        visited.insert((position.x, position.y)); // hack to break out of nested loop
+    }
+
+    let block_count = instruction.blocks;
+    for _ in 0..block_count {
+        match heading {
+            Heading::North => {
+                position.y += 1;
+            }
+            Heading::East => {
+                position.x += 1;
+            }
+            Heading::South => {
+                position.y -= 1;
+            }
+            Heading::West => {
+                position.x -= 1;
+            }
+        }
+        if visited.contains(&(position.x, position.y)) {
+            return None // hack to break out of nested loop
+        }
+        visited.insert((position.x, position.y));
+    }
+
+    Some(heading)
 }
 
 fn main() {
-    let input = read_input().unwrap();
-    let directions = parse_instructions(input).unwrap();
+    // transform error reading file into string
+    fn stringify(e: io::Error) -> String {
+        format!("{}", e)
+    }
 
-    let start = Point { x: 0, y: 0 };
-    let end = follow_directions(&directions, &start, Direction::North);
+    match read_input().map_err(stringify).and_then(parse_instructions).and_then(find_distance) {
+        Ok(distance) => println!("distance = {}", distance),
+        Err(_) => println!("error"),
+    }
 
-    println!("{}", distance(&start, &end));
+    match read_input()
+        .map_err(stringify)
+        .and_then(parse_instructions)
+        .and_then(|i| find_revisited_distance(i).ok_or("no convergence".to_string())) {
+        Ok(distance) => println!("distance = {}", distance),
+        Err(e) => println!("error: {}", e),
+    }
 }
 
 #[test]
-fn test_distance() {
-    let origin = Point { x: 0, y: 0 };
-    assert_eq!(5, distance(&origin, &Point { x: 2, y: 3 }));
-    assert_eq!(2, distance(&origin, &Point { x: 0, y: -2 }));
-    assert_eq!(12, distance(&origin, &Point { x: 10, y: 2 }));
-}
-
-#[test]
-fn test_read_directions() {
-    assert_eq!(parse_instructions("R2, R2, R2".to_string()).unwrap(),
-               vec![Instruction {
-                        turn: Turn::Right,
-                        distance: 2,
-                    }; 3]);
-    assert_eq!(parse_instructions("L32, R128".to_string()).unwrap(),
-               vec![Instruction {
-                        turn: Turn::Left,
-                        distance: 32,
-                    },
-                    Instruction {
-                        turn: Turn::Right,
-                        distance: 128,
-                    }]);
-}
-
-#[test]
-fn test_follow_directions() {
-    let origin = Point { x: 0, y: 0 };
+fn test_find_distance() {
     assert_eq!(5,
-               distance(&follow_directions(&parse_instructions("R2, L3".to_string()).unwrap(),
-                                           &origin,
-                                           Direction::North),
-                        &origin));
-    assert_eq!(2,
-               distance(&follow_directions(&parse_instructions("R2, R2, R2".to_string())
-                                               .unwrap(),
-                                           &origin,
-                                           Direction::North),
-                        &origin));
+               parse_instructions("R2, L3".to_string()).and_then(find_distance).unwrap());
     assert_eq!(12,
-               distance(&follow_directions(&parse_instructions("R5, L5, R5, R3".to_string())
-                                               .unwrap(),
-                                           &origin,
-                                           Direction::North),
-                        &origin));
+               parse_instructions("R5, L5, R5, R3".to_string()).and_then(find_distance).unwrap());
+    assert_eq!(2,
+               parse_instructions("R2, R2, R2".to_string()).and_then(find_distance).unwrap());
+}
+
+#[test]
+fn test_first_visited_twice() {
+    assert_eq!(4,
+               parse_instructions("R8, R4, R4, R8".to_string())
+                   .and_then(|i| find_revisited_distance(i).ok_or("error".to_string()))
+                   .unwrap());
+    assert_eq!(0,
+               parse_instructions("L2, R2, R2, R3, L6".to_string())
+                   .and_then(|i| find_revisited_distance(i).ok_or("error".to_string()))
+                   .unwrap());
 }

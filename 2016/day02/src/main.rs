1449cbc0398d4f95a6017327b6f1d271051259c7
Mike Killpack <mkillpack@gmail.com>
2016-12-05 22:27:08 -0700

    day02: add solution for part two

--- 2016/day02/src/main.rs
+++ 2016/day02/src/main.rs
@@ -7,13 +7,20 @@ static SQUARE_KEYPAD: [[u32; 3]; 3] = [[1, 2, 3],
                                        [4, 5, 6],
                                        [7, 8, 9]];
 
+#[cfg_attr(rustfmt, rustfmt_skip)]
+static DIAMOND_KEYPAD: [[char; 5]; 5] = [['0', '0', '1', '0', '0'],
+                                        ['0', '2', '3', '4', '0'],
+                                        ['5', '6', '7', '8', '9'],
+                                        ['0', 'A', 'B', 'C', '0'],
+                                        ['0', '0', 'D', '0', '0']];
+
 fn read_input() -> Result<String, io::Error> {
     let mut input = String::new();
     File::open("input")?.read_to_string(&mut input)?;
     Ok(input)
 }
 
-fn get_combination(instructions: String) -> Result<String, &'static str> {
+fn get_combination(instructions: &String) -> Result<String, &'static str> {
     let mut combination = String::new();
     let mut x = 1;
     let mut y = 1;
@@ -50,17 +57,64 @@ fn get_combination(instructions: String) -> Result<String, &'static str> {
     Ok(combination)
 }
 
+fn get_diamond_combination(instructions: &String) -> Result<String, &'static str> {
+    let mut x = 0;
+    let mut y = 2;
+    let mut combination = String::new();
+
+    for line in instructions.lines() {
+        for instr in line.chars() {
+            match instr {
+                'U' => {
+                    if y > 0 && DIAMOND_KEYPAD[y - 1][x] != '0' {
+                        y -= 1;
+                    }
+                }
+                'D' => {
+                    if y < DIAMOND_KEYPAD.len() - 1 && DIAMOND_KEYPAD[y + 1][x] != '0' {
+                        y += 1;
+                    }
+                }
+                'L' => {
+                    if x > 0 && DIAMOND_KEYPAD[y][x - 1] != '0' {
+                        x -= 1;
+                    }
+                }
+                'R' => {
+                    if x < DIAMOND_KEYPAD.len() - 1 && DIAMOND_KEYPAD[y][x + 1] != '0' {
+                        x += 1;
+                    }
+                }
+                _ => return Err("invalid instruction in input!"),
+            }
+        }
+        combination.push(DIAMOND_KEYPAD[y][x]);
+    }
+
+    Ok(combination)
+}
+
+fn print_combination(result: Result<String, &'static str>) {
+    match result {
+        Ok(combination) => println!("{}", combination),
+        Err(e) => println!("Error! {}", e),
+    }
+}
+
 fn main() {
     let input = read_input().unwrap();
-    match get_combination(input) {
-        Ok(combination) => println!("{}", combination),
-        Err(e) => println!("oh noes! {}", e),
-    }
+
+    print!("First combination:  ");
+    print_combination(get_combination(&input));
+
+    print!("{}", "Second combination: ");
+    print_combination(get_diamond_combination(&input));
 }
 
 #[test]
 fn test_instructions() {
-    let combination = get_combination(5, 3, "ULL\nRRDDD\nLURDL\nUUUUD".to_string()).unwrap();
+    let instructions = "ULL\nRRDDD\nLURDL\nUUUUD".to_owned();
 
-    assert_eq!("1985", combination);
+    assert_eq!("1985", get_combination(&instructions).unwrap());
+    assert_eq!("5DB3", get_diamond_combination(&instructions).unwrap());
 }

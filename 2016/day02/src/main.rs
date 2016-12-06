749971a379f2de914ac00e9ead06c85985684c8c
Mike Killpack <mkillpack@gmail.com>
2016-12-05 20:29:43 -0700

    day02: refactor to use 2D array

--- 2016/day02/src/main.rs
+++ 2016/day02/src/main.rs
@@ -2,56 +2,57 @@ use std::fs::File;
 use std::io;
 use std::io::Read;
 
+#[cfg_attr(rustfmt, rustfmt_skip)]
+static SQUARE_KEYPAD: [[u32; 3]; 3] = [[1, 2, 3],
+                                       [4, 5, 6],
+                                       [7, 8, 9]];
+
 fn read_input() -> Result<String, io::Error> {
     let mut input = String::new();
     File::open("input")?.read_to_string(&mut input)?;
     Ok(input)
 }
 
-fn get_combination(start_at: u32,
-                   dimension: u32,
-                   instructions: String)
-                   -> Result<String, &'static str> {
-    let mut position = start_at;
+fn get_combination(instructions: String) -> Result<String, &'static str> {
     let mut combination = String::new();
+    let mut x = 1;
+    let mut y = 1;
 
     for line in instructions.lines() {
         for instr in line.chars() {
             match instr {
                 'U' => {
-                    if position > dimension {
-                        position -= dimension;
+                    if y > 0 {
+                        y -= 1;
                     }
                 }
                 'D' => {
-                    if position <= (dimension * dimension - dimension) {
-                        position += dimension;
+                    if y < SQUARE_KEYPAD.len() - 1 {
+                        y += 1;
                     }
                 }
                 'L' => {
-                    if position % dimension != 1 {
-                        position -= 1;
+                    if x > 0 {
+                        x -= 1;
                     }
                 }
                 'R' => {
-                    if position % dimension > 0 {
-                        position += 1;
+                    if x < SQUARE_KEYPAD.len() - 1 {
+                        x += 1;
                     }
                 }
                 _ => return Err("invalid instruction in input!"),
             }
         }
-        combination.push(std::char::from_digit(position, 10).unwrap());
+        combination.push(std::char::from_digit(SQUARE_KEYPAD[y][x], 10).unwrap());
     }
 
     Ok(combination)
 }
 
 fn main() {
-    let dimension = 3;
-
     let input = read_input().unwrap();
-    match get_combination(5, dimension, input) {
+    match get_combination(input) {
         Ok(combination) => println!("{}", combination),
         Err(e) => println!("oh noes! {}", e),
     }

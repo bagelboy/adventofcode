02f636f3eadcf8cf5c059b4c41791b033b808755
Mike Killpack <mkillpack@gmail.com>
2016-12-06 01:14:01 -0700

    day05: add solution for part two

--- 2016/day05/src/main.rs
+++ 2016/day05/src/main.rs
@@ -22,12 +22,44 @@ fn find_password(input: &str) -> String {
     password
 }
 
+fn find_second_password(input: &str) -> String {
+    let mut hasher = Md5::new();
+    let mut password = ['_'; 8];
+    let mut found = 0;
+
+    for i in 0.. {
+        hasher.reset();
+        hasher.input_str(input);
+        hasher.input_str(&i.to_string());
+        let result = hasher.result_str();
+        if result.starts_with("00000") {
+            let mut chars = result.chars();
+            let position = chars.nth(5).unwrap();
+            let character = chars.next().unwrap();
+            if let Some(index) = position.to_digit(10) {
+                let actual_index = index as usize;
+                if index < 8 && password[actual_index] == '_' {
+                    password[actual_index] = character;
+                    found += 1;
+                }
+            }
+            if found == 8 {
+                break;
+            }
+        }
+    }
+
+    password.iter().cloned().collect::<String>()
+}
+
 fn main() {
-    println!("{}", find_password("ffykfhsq"));
+    println!("first door:  {}", find_password("ffykfhsq"));
+    println!("second door: {}", find_second_password("ffykfhsq"));
 }
 
 #[test]
 fn test_find_password() {
     // run `cargo test --release` or this takes a LONG time!
     assert_eq!("18f47a30", find_password("abc"));
+    assert_eq!("05ace8e3", find_second_password("abc"));
 }

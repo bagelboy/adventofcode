1cfcd8f9050a9a48f1f62edba6ec088281a7e5c8
Mike Killpack <mkillpack@gmail.com>
2017-12-17 22:37:05 -0700

    2017: Add part two solution for day17

--- 2017/day17/src/main.rs
+++ 2017/day17/src/main.rs
@@ -1,17 +1,38 @@
-use std::collections::VecDeque;
-
 fn main() {
     let steps = 394;
 
-    let mut buffer = VecDeque::with_capacity(2018);
-    buffer.push_back(0);
-    let mut current_position = 0;
-    for i in 0..2017 {
-        let new_position = (current_position + steps) % buffer.len() + 1;
-        buffer.insert(new_position, i + 1);
-        current_position = new_position;
+    // part one
+    {
+        use std::collections::VecDeque;
+
+        let iterations = 2017;
+
+        let mut buffer = VecDeque::with_capacity(iterations + 1);
+        buffer.push_back(0);
+        let mut current_position = 0;
+        for i in 1..(iterations + 1) {
+            let new_position = (current_position + steps) % i + 1;
+            buffer.insert(new_position, i);
+            current_position = new_position;
+        }
+
+        let index = buffer.iter().position(|&v| v == iterations).unwrap() + 1;
+        println!("value after {}: {}", iterations, buffer[index]);
     }
 
-    let index = buffer.iter().position(|&v| v == 2017).unwrap() + 1;
-    println!("value after 2017: {}", buffer[index]);
+    // part two
+    {
+        let mut last_inserted = 0;
+        let mut current_position = 0;
+
+        for i in 1..(50_000_001) {
+            let new_position = (current_position + steps) % i;
+            if new_position == 0 {
+                last_inserted = i;
+            }
+            current_position = new_position + 1;
+        }
+
+        println!("value after 0: {}", last_inserted);
+    }
 }

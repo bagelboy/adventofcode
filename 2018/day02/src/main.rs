4af6de4f2a4c504aeb25c363e73b04f79383550b
Mike Killpack <mkillpack@gmail.com>
2018-12-02 03:44:39 -0700

    Add naive solution for day two, part two

--- 2018/day02/src/main.rs
+++ 2018/day02/src/main.rs
@@ -1,3 +1,6 @@
+extern crate itertools;
+
+use itertools::Itertools;
 use std::collections::HashMap;
 use std::fs;
 
@@ -30,8 +33,49 @@ fn test_compute_checksum() {
     );
 }
 
+fn compare(left: &str, right: &str) -> bool {
+    if left.len() != right.len() {
+        return false;
+    }
+    let b1 = left.as_bytes();
+    let b2 = right.as_bytes();
+    let mut differences = 0;
+    for i in 0..b1.len() {
+        if b1[i] != b2[i] {
+            differences += 1;
+        }
+        if differences > 1 {
+            return false;
+        }
+    }
+    true
+}
+
+fn find_common_letters<'a>(ids: &'a [&str]) -> Option<String> {
+    for (left, right) in ids.iter().tuple_combinations() {
+        if compare(left, right) {
+            return Some(
+                left.chars()
+                    .zip(right.chars())
+                    .filter_map(|(a, b)| if a == b { Some(a) } else { None })
+                    .collect::<String>(),
+            );
+        }
+    }
+    None
+}
+
+#[test]
+fn test_find_common_letters() {
+    assert_eq!(
+        Some("fgij".to_string()),
+        find_common_letters(&["abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"])
+    );
+}
+
 fn main() {
     let input = fs::read_to_string("input").expect("cannot read input");
     let ids: Vec<_> = input.lines().collect();
     println!("checksum: {}", compute_checksum(&ids));
+    println!("common letters: {}", find_common_letters(&ids).unwrap());
 }
